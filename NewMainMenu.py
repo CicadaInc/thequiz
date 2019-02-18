10049611bb7a25e608cee6afbb818c85f814d1ac
Mark <mark.2406@yandex.ru>
2019-02-18 15:44:04 +0500

    fix some bugs and delete usnused filess

--- NewMainMenu.py
+++ NewMainMenu.py
@@ -28,7 +28,6 @@ class NewMainMenu:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     x, y = event.pos
-                    x += 12
                     if self.coinRect.collidepoint((x, y)):
                         push = True
                         x_, y_ = event.pos[0] - self.x, event.pos[1] - self.y
